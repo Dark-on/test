1b798fb39ac614732cd79b1a63aeb1388320b879
201dreamers <201dreamers@gmail.com>
2021-06-18 04:17:01 +0300

    calendar 1

--- src/ui/calendar_layout.py
+++ src/ui/calendar_layout.py
@@ -1,38 +1,13 @@
 import calendar
 from datetime import date
 
-from kivy.core import text
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager
-from kivy.uix.scrollview import ScrollView
 from kivy.lang import Builder
-from kivy.properties import StringProperty
 from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.list import ILeftBody, MDList, ThreeLineListItem
-from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.button import MDFloatingActionButton
-from kivymd.uix.bottomnavigation import MDBottomNavigationItem
-from kivymd.uix.list import OneLineIconListItem
-from kivy.uix.scrollview import ScrollView
-from kivymd.uix.selectioncontrol import MDCheckbox
 
 from src.data_provider import dpr
 from config import Config
-
-
-# text_info = (
-        #     f"[color=#707070]Id:[/color] {goal.id_}\n"
-        #     f"[color=#707070]Title:[/color] {goal.title}\n"
-        #     f"[color=#707070]Vote average:[/color] {goal.vote_average}\n\n"
-        #     f"[color=#707070]Overview:[/color] {goal.details.overview}\n"
-        #     f"[color=#707070]Tagline:[/color] {goal.details.tagline}\n"
-        #     f"[color=#707070]Status:[/color] {goal.details.status}\n"
-        #     f"[color=#707070]Genres:[/color] {goal.details.genres}\n"
-        # )
 
 
 Builder.load_file(f"{Config.TEMPLATES_DIR}/calendar_layout.kv")
@@ -45,6 +20,7 @@ class CalendarCell(MDBoxLayout):
         self.add_widget(
             MDLabel(
                 text=str(text),
+                markup=True,
                 halign=halign,
                 valign=valign,
                 padding=(5, 5)
@@ -75,23 +51,26 @@ class CalendarLayout(MDGridLayout):
         for week in calendar_matrix:
             for day_number in week:
                 day_number = "" if day_number == 0 else day_number
-                color = self._get_day_color(day_number)
+                color, choice = self._get_day_color_and_choice(day_number)
                 self.add_widget(
                     CalendarCell(
-                        text=day_number,
+                        text=f"{day_number}\n[b]{choice}[/b]",
                         color=color,
                         halign="left",
                         valign="top"
                     )
                 )
 
-    def _get_day_color(self, day):
+    def _get_day_color_and_choice(self, day):
         records = dpr.get_records(self.goal.get("name"))
+        color = (1, 1, 1, 0)
+        choice = ""
         for record in records:
             if record.get("date") == f"{self.year}-{self.month:02}-{day}":
-                return (0.58, 0.82, 0.56, 1)
-        return (1, 1, 1, 0)
-
+                color = (0.58, 0.82, 0.56, 1)
+                choice = record.get("choice") if record.get("choice") else "Note"
+                break
+        return color, choice
 
     def next_month(self):
         self._offset += 1
